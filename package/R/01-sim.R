75eb55e4b6a271d9efe39521f2f3284d1fefea33
Sebastian <wahani@gmail.com>
2014-03-10 19:09:59 +0100

    bugfix in sim.sim_base

--- package/R/01-sim.R
+++ package/R/01-sim.R
@@ -2,7 +2,7 @@ setGeneric("sim", function(x, ...) standardGeneric("sim"))
 
 setMethod("sim", c(x = "sim_base"),
           function(x, ...) {
-            smstp_objects <- lapply(list(...), function(fun) fun(sim_base))
+            smstp_objects <- lapply(list(...), function(fun) fun(x))
             results <- lapply(smstp_objects, sim_generate)
             Reduce(add, results)
           })
