6fe9f5cb0f28675f5166ddc0d68fe46c4412d001
Sebastian <wahani@gmail.com>
2014-05-29 21:24:13 +0200

    #27 updated agg_standard

--- R/03-agg.R
+++ R/03-agg.R
@@ -4,7 +4,7 @@
 #' 
 #' @param splitVars variable names as character to split the data.
 #' 
-#' @details This function follows the split-apply-combine idiom. Each data set is split by the defined variables. Then the variables within each subset are aggregated (reduced to one row). Logical variables are reduced by \code{\link{any}}; for characters and factors the most frequent value is taken; and for numerics the mean (removing NAs).
+#' @details This function follows the split-apply-combine idiom. Each data set is split by the defined variables. Then the variables within each subset are aggregated (reduced to one row). Logical variables are reduced by \code{\link{any}}; for characters and factors dummy variables are created and the aggregate is the mean of each dummy; and for numerics the mean (removing NAs).
 #' 
 #' @seealso \code{\link{sim_agg}}
 #' @export
@@ -13,6 +13,14 @@
 #' sim_base_standard() %+% sim_gen_fe() %+% sim_gen_e() %+% sim_agg(agg_standard())
 agg_standard <- function(splitVars = "idD") {
   function(dat) {
+    # character > factor > dummies (numeric)
+    dat[sapply(dat, is.character)] <- dat[sapply(dat, is.character)] %>% lapply(as.factor)
+    dummies <- names(dat)[sapply(dat, is.factor)] %>% 
+      lapply(function(fac) {
+        paste("~ -1", fac, sep = " + ") %>% as.formula %>% 
+          model.matrix(data=dat) %>% as.data.frame
+    }) %>% do.call(what=cbind)
+    dat <- c(dat[!sapply(dat, is.factor)], dummies) %>% as.data.frame
     # Delete vars:
     dat <- dat[!(names(dat) %in% c("idU"))]
     # split:
@@ -23,14 +31,9 @@ agg_standard <- function(splitVars = "idD") {
              dfOut <- df[1,]
              # logicals
              dfOut[sapply(df, is.logical)] <- lapply(df[sapply(df, is.logical)], any)
-             # character
-             dfOut[sapply(df, is.character)] <- lapply(df[sapply(df, is.character)], 
-                                                       function(char) names(sort(table(char), decreasing=TRUE)[1]))
-             # factors
-             dfOut[sapply(df, is.factor)] <- lapply(df[sapply(df, is.factor)], 
-                                                    function(fac) factor(names(sort(table(fac), decreasing=TRUE)[1])))
              # numeric
-             dfOut[sapply(df, is.numeric)] <- lapply(df[sapply(df, is.numeric)], mean, na.rm = TRUE)
+             dfOut[sapply(df, function(var) is.numeric(var) | is.integer(var))] <- 
+               lapply(df[sapply(df, is.numeric)], mean, na.rm = TRUE)
              
              dfOut
            })
--- tests/testthat/test-sim_agg.R
+++ tests/testthat/test-sim_agg.R
@@ -2,20 +2,23 @@ context("sim_agg")
 test_that("Basic functionality", {
   setup <- sim_setup(sim_base_standard(), R=1) %+% sim_gen_e() %+% sim_gen_fe() %+% 
     sim_calc(calc_var("y", funList=list("length" = length), newName="N")) %+% sim_agg() %+%
-    sim_calc(calc_var("y", funList = list("make_factor" = function(x) factor("a"), 
-                                          "make_character" = function(x) "b")))
+    sim_calc(calc_var("y", funList = list("make_factor" = function(x) factor(letters[1:2]), 
+                                          "make_character" = function(x) letters[1:2])))
   
   dat <- sim(setup)[[1]]
   expect_that(nrow(dat), equals(100))
   expect_that(dat$idU, is_a("NULL"))
+  expect_that(dat$yMake_factor, is_a("NULL"))
+  expect_that(dat$yMake_character, is_a("NULL"))
   expect_that(dat$idD, is_a("numeric"))
   expect_that(dat$e, is_a("numeric"))
   expect_that(dat$x, is_a("numeric"))
   expect_that(dat$y, is_a("numeric"))
   expect_that(dat$N, is_a("numeric"))
-  expect_that(dat$yMake_factor, is_a("factor"))
-  expect_that(dat$yMake_character, is_a("character"))
+  expect_that(dat$yMake_factora, is_a("numeric"))
+  expect_that(dat$yMake_factorb, is_a("numeric"))
+  expect_that(dat$yMake_charactera, is_a("numeric"))
+  expect_that(dat$yMake_characterb, is_a("numeric"))
   expect_that(dat$idR, is_a("integer"))
-  expect_that(dat$simName, is_a("character"))
-  
+  expect_that(dat$simName, is_a("character"))  
 })
