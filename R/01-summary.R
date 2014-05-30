4f97a2d6b99b8d733c2bc4a2968801c8b5268a0e
Sebastian <wahani@gmail.com>
2014-05-30 17:47:06 +0200

    #11 summary for sim_setup - version 0.4.0

---
+++ R/01-summary.R
@@ -0,0 +1,40 @@
+#' Summary for a sim_setup
+#' 
+#' Reports a summary of the simulation setup.
+#' 
+#' @param object a simulation setup constructed with \code{\link{sim_setup}}.
+#' @param ... nothing to pass to here.
+#' 
+#' @export
+#' @method summary sim_setup
+#' 
+#' @examples
+#' summary(sim_lm())
+summary.sim_setup <- function(object, ...) {
+  componentList <- S3Part(object, strictS3=TRUE)
+  cat("General Information about", object@simName, "simulation set-up:\n")
+  cat("# of runs:", object@R, "\n")
+  cat("# of sim_gen:", 
+      sum(is.sim_gen_virtual(componentList) | is.sim_genData(componentList)),
+      "\n")
+  cat("# of sim_calc (population):", 
+      sum(is.sim_cpopulation(componentList)), "\n")
+  cat("# of sim_sample:",
+      sum(is.sim_sample(componentList)), "\n")
+  cat("# of sim_calc (sample):", 
+      sum(is.sim_csample(componentList)), "\n")
+  cat("# of sim_agg:",
+      sum(is.sim_agg(componentList)), "\n")
+  cat("# of sim_calc (aggregate):", 
+      sum(is.sim_cagg(componentList)), "\n")
+  
+  cat("\nApproximating the expected duration:\n")
+  cat("A single run takes ... ")
+  tmp <- system.time(dat <- as.data.frame(object))
+  cat(tmp["elapsed"], "seconds.", object@R, "*", tmp["elapsed"], "=", 
+      object@R * tmp["elapsed"], "seconds.\n")
+  
+  cat("\nHead of the data:\n")
+  print(head(dat))
+  invisible(NULL)
+}
