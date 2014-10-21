05ac6e8bed5a6d85f21f444a44545bfe55a55f3b
Sebastian <wahani@gmail.com>
2014-10-21 22:04:24 +0200

    git for staticdocs

--- devel/makeStaticDoc.R
+++ devel/makeStaticDoc.R
@@ -5,3 +5,5 @@ staticdocs::build_site()
 file.remove(
   list.files(path = "vignettes", pattern = "*.html", full.names = TRUE))
 
+system('git add -f inst/web && git commit -m "Initial dist subtree commit"')
+system('git subtree push --prefix inst/web origin gh-pages')
