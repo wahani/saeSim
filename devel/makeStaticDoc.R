df97dae501a95e3f87cf8bd1888bc33d4bcf30e8
Sebastian <wahani@gmail.com>
2014-10-22 11:21:17 +0200

    gh-pages subtree commit

--- devel/makeStaticDoc.R
+++ devel/makeStaticDoc.R
@@ -3,7 +3,13 @@ document()
 build_vignettes()
 staticdocs::build_site()
 file.remove(
-  list.files(path = "vignettes", pattern = "*.html", full.names = TRUE))
+  list.files(path = "vignettes", pattern = "*.html", full.names = TRUE)
+  )
 
-system('git add -f inst/web && git commit -m "Initial dist subtree commit"')
+# Fix for vignettes -- not correctly build by staticdocs
+file.copy(list.files(path = "inst/doc/", pattern = "*.html", full.names = TRUE),
+          list.files(path = "inst/web/vignettes/", pattern = "*.html", full.names = TRUE),
+          overwrite = TRUE)
+
+system('git add -f inst/web && git commit -m "gh-pages subtree commit"')
 system('git subtree push --prefix inst/web origin gh-pages')
