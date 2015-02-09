026fad74580f53c177d3c2a568ca78167a043d2d
Sebastian <wahani@gmail.com>
2015-02-09 16:02:54 +0100

    bugfix in example

--- R/03-sample.R
+++ R/03-sample.R
@@ -16,8 +16,8 @@
 #' @examples
 #' sim_base_lm() %>% sim_sample(sample_number(5))
 #' sim_base_lm() %>% sim_sample(sample_fraction(0.5))
-#' sim_base_lm() %>% sim_sample(sample_cluster_number(5))
-#' sim_base_lm() %>% sim_sample(sample_cluster_fraction(0.5))
+#' sim_base_lm() %>% sim_sample(sample_cluster_number(5, groupVars = "idD"))
+#' sim_base_lm() %>% sim_sample(sample_cluster_fraction(0.5, groupVars = "idD"))
 sample_fraction <- function(size, replace = FALSE, weight = NULL, groupVars = NULL) {
   force(size); force(replace); force(weight); force(groupVars)
   function(dat) {
