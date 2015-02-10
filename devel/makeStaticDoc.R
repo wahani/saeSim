dbe6ad600c95d25987a76e3524d5a1adfa5b593f
Sebastian <wahani@gmail.com>
2015-02-10 10:58:39 +0100

    makeStaticDocs update

--- devel/makeStaticDoc.R
+++ devel/makeStaticDoc.R
@@ -5,9 +5,9 @@ build_vignettes()
 checkResult <- check()
 
 if(checkResult) {
-  system("rm -r inst/web") # needs to be deleted because the vignette was not updated
+  system("rm -r inst/web/*") # needs to be deleted because the vignette was not updated
   staticdocs::build_site()
-  #system('git push origin --delete gh-pages')
+#   system('git push origin --delete gh-pages')
   system('git add -f inst/web && git commit -m "gh-pages subtree commit"')
   system('git subtree push --prefix inst/web origin gh-pages')
 }
