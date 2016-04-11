fb6835b4d438af05b242d0d53cdc3b4e2301e2b2
Sebastian <wahani@gmail.com>
2016-04-11 15:04:51 +0200

    fix for return value of preconf comp funs

--- R/03-sim_comp_preconf.R
+++ R/03-sim_comp_preconf.R
@@ -7,23 +7,27 @@
 #' @export
 #' @rdname sim_comp_preconf
 sim_comp_n <- function(simSetup) {
-  sim_comp_sample(simSetup, function(dat) mutate_(dat, n = nrow(dat)), by = "idD")
+  sim_comp_sample(simSetup, function(dat) 
+    as.data.frame(mutate_(dat, n = nrow(dat))), by = "idD")
 }
 
 #' @export
 #' @rdname sim_comp_preconf
 sim_comp_N <- function(simSetup) {
-  sim_comp_pop(simSetup, function(dat) mutate_(dat, N = nrow(dat)), by = "idD")
+  sim_comp_pop(simSetup, function(dat) 
+    as.data.frame(mutate_(dat, N = nrow(dat))), by = "idD")
 }
 
 #' @export
 #' @rdname sim_comp_preconf
 sim_comp_popMean <- function(simSetup) {
-  sim_comp_pop(simSetup, function(dat) mutate_(dat, popMean = "mean(y)"), by = "idD")
+  sim_comp_pop(simSetup, function(dat) 
+    as.data.frame(mutate_(dat, popMean = "mean(y)")), by = "idD")
 }
 
 #' @export
 #' @rdname sim_comp_preconf
 sim_comp_popVar <- function(simSetup) {
-  sim_comp_pop(simSetup, function(dat) mutate_(dat, popVar = "var(y)"), by = "idD")
+  sim_comp_pop(simSetup, function(dat) 
+    as.data.frame(mutate_(dat, popVar = "var(y)")), by = "idD")
 }
